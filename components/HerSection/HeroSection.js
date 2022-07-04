b16f3d83e7d6ee97e99dd80768d9c9a4d5d8bfe8
hayaIbra <hayaibra217@gmail.com>
2022-07-04 14:53:52 +0300

    styling hero section

--- components/HerSection/HeroSection.js
+++ components/HerSection/HeroSection.js
@@ -1,10 +1,54 @@
 import { Stack, Box, Typography } from "@mui/material";
 import React from "react";
 import { HeroBox, EllipseBox } from "./Style";
+import Image from "next/image";
 
 function HeroSection() {
   return (
     <HeroBox>
+      <Stack
+        direction="row"
+        spacing={2}
+        display="flex"
+        sx={{
+          alignItems: "center",
+          height: "100%",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <Box sx={{ paddingLeft: "85px" }}>
+          <Typography
+            variant="h1"
+            color="white"
+            fontWeight="bold"
+            fontSize="100px"
+          >
+            Hello,
+          </Typography>
+          <Typography
+            variant="h1"
+            color="white"
+            fontWeight="bold"
+            fontSize="100px"
+          >
+            I am Haya
+          </Typography>
+          <Typography
+            variant="h6"
+            color="white"
+            sx={{
+              letterSpacing: "4.5px",
+              fontSize: "22px",
+              fontWeight: "bold",
+            }}
+          >
+            Front-End Developer
+          </Typography>
+        </Box>
+        <Box sx={{ zIndex: 1000 }}>
+          <Image src={require("../../public/img/girl-1.png")} width="550" />
+        </Box>
+      </Stack>
       <EllipseBox />
     </HeroBox>
   );
